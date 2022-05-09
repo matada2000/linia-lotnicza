1da87c193eb08ccd91091ca6104bb118ab273118
matada2000 <matada200060@gmail.com>
2022-05-09 21:35:12 +0200

    v1.10 - users i admins

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +24,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix'=>'admin', 'middleware'=>['isAdmin','auth']], function(){
+    Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+    Route::get('profile',[AdminController::class,'profile'])->name('admin.profile');
+    Route::get('settings',[AdminController::class,'settings'])->name('admin.settings');
+
+   
+});
+
+Route::group(['prefix'=>'user', 'middleware'=>['isUser','auth']], function(){
+    Route::get('dashboard',[UserController::class,'index'])->name('user.dashboard');
+    Route::get('profile',[UserController::class,'profile'])->name('user.profile');
+    Route::get('settings',[UserController::class,'settings'])->name('user.settings');
+
+});
+
+
